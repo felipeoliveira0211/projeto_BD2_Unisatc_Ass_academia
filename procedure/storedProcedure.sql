d787bcf1566d9950c34ce3adbda939b281aba676
GustavoRossa <gustavorossa1@hotmail.com>
2026-06-22 12:35:34 -0300

    storedProcedure
    Versão final da Procedure.

--- procedure/storedProcedure.sql
+++ procedure/storedProcedure.sql
@@ -1,46 +1,36 @@
-DELIMITER $$
-
-CREATE PROCEDURE sp_vender_plano(
-    IN p_id_aluno INT,
-    IN p_id_plano INT,
-    IN p_id_func_venda INT
+CREATE PROCEDURE sp_relatorio_frequencia_aluno
+(
+    @data_inicio DATE,
+    @data_fim DATE,
+    @id_aluno INT = NULL
 )
+AS
 BEGIN
-    DECLARE v_valor_mensal DECIMAL(10,2);
-    DECLARE v_duracao_meses TINYINT;
-    DECLARE v_id_assinatura INT;
-    DECLARE v_data_vencimento DATE;
+    SET NOCOUNT ON;
 
-    -- Busca o valor e a duração do plano escolhido
-    SELECT valor_mensal, duracao_meses 
-    INTO v_valor_mensal, v_duracao_meses
-    FROM planos 
-    WHERE id = p_id_plano;
-
-    -- Insere a nova assinatura (Inicia hoje)
-    INSERT INTO assinaturas (
-        id_aluno, id_plano, id_funcionario_venda,
-        data_inicio, status_assinatura, renovacao_automatica
-    ) VALUES (
-        p_id_aluno, p_id_plano, p_id_func_venda,
-        CURRENT_DATE(), 'A', 1
-    );
-
-    -- Pega o ID da assinatura que acabou de ser gerada
-    SET v_id_assinatura = LAST_INSERT_ID();
-
-    -- Define o vencimento da primeira fatura para daqui a 5 dias (exemplo)
-    SET v_data_vencimento = DATE_ADD(CURRENT_DATE(), INTERVAL 5 DAY);
-
-    -- Gera a primeira fatura de pagamento para essa assinatura
-    INSERT INTO faturas_pagamento (
-        id_assinatura, data_vencimento, valor_fatura, 
-        status_pagamento, mes_referencia, ano_referencia
-    ) VALUES (
-        v_id_assinatura, v_data_vencimento, v_valor_mensal, 
-        'P', MONTH(CURRENT_DATE()), YEAR(CURRENT_DATE())
-    );
-
-END$$
-
-DELIMITER ;
+    SELECT
+        a.id AS id_aluno,
+        a.nome_completo,
+        p.nome AS plano,
+        COUNT(ac.id) AS total_acessos,
+        MIN(ac.data_hora_entrada) AS primeiro_acesso_periodo,
+        MAX(ac.data_hora_entrada) AS ultimo_acesso_periodo
+    FROM alunos a
+    INNER JOIN assinaturas ass
+        ON ass.id_aluno = a.id
+    INNER JOIN planos p
+        ON p.id = ass.id_plano
+    LEFT JOIN acessos_catraca ac
+        ON ac.id_aluno = a.id
+        AND CAST(ac.data_hora_entrada AS DATE) BETWEEN @data_inicio AND @data_fim
+        AND ac.sentido_acesso = 'E'
+    WHERE a.ativo = 1
+      AND ass.status_assinatura = 'A'
+      AND (@id_aluno IS NULL OR a.id = @id_aluno)
+    GROUP BY
+        a.id,
+        a.nome_completo,
+        p.nome
+    ORDER BY total_acessos DESC;
+END;
+GO
